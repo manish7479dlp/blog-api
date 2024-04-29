a1e41d1ad2bf909fb3dd983474224f1de93af9bf
manish7479dlp <manish7479dlp@gmail.com>
2024-04-29 16:09:42 +0530

    style: formate the code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,24 +1,29 @@
 const express = require("express");
-const { userRegistration, userLogin, userProfile, userLogout, userUpdate } = require("../controllers/authController");
-const authMiddleware = require("../middleware/authMiddleware")
+const {
+  userRegistration,
+  userLogin,
+  userProfile,
+  userLogout,
+  userUpdate,
+} = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 //api routes
 const route = express.Router();
 
 //user register  || POST
-route.post("/user/register" , userRegistration)
+route.post("/user/register", userRegistration);
 
 //user login || POST
-route.post("/user/login" , userLogin)
+route.post("/user/login", userLogin);
 
 //user profile ||   GET
-route.get("/user" , authMiddleware , userProfile)
+route.get("/user", authMiddleware, userProfile);
 
 //user logout
 
-route.get("/user/logout",authMiddleware , userLogout);
+route.get("/user/logout", authMiddleware, userLogout);
 
-route.put("/user/update" , authMiddleware , userUpdate);
-
+route.put("/user/update", authMiddleware, userUpdate);
 
 module.exports = route;

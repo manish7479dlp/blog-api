e726f26cce4ee2caf9947ffe5229a66487cd044b
manish7479dlp <manish7479dlp@gmail.com>
2024-04-30 14:32:40 +0530

    style: update order of the cors policy in index.js file

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-require("./config/dotenvConfig")
-const connectDb = require("./config/dbConfig")
-const userRoute = require("./routes/userRoute")
-const postRoute = require("./routes/postRoute")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("./config/dotenvConfig");
+const connectDb = require("./config/dbConfig");
+const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
 
 //api object
 const app = express();
 
-const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL;
 const port = process.env.PORT || 8000;
 
-
 //connect db
-connectDb(dbUrl)
+connectDb(dbUrl);
 
-const originURLs = ["http://localhost:3000","https://manish7479dlp-blog.netlify.app"]
+const originURLs = [
+  "http://localhost:3000",
+  "https://manish7479dlp-blog.netlify.app",
+];
 //middleware
+app.use(cors({ origin: originURLs, credentials: true }));
 app.use(express.json());
-app.use(cors({origin: originURLs , credentials: true}))
 app.use(cookieParser());
 // app.use("/uploads" , express.static(__dirname + '/uploads'))
-app.use(express.static("public"))
-
-
-//routes
-app.use("/api/v1" , userRoute)
-app.use("/api/v1" , postRoute)
+app.use(express.static("public"));
 
 //routes
-app.use("/" , (req , res) => {
-    try {
-       res.send("hlw from the other side....") 
-    } catch (error) {
-        console.log(error);
-        res.send(error)
-    }
-})
+app.use("/api/v1", userRoute);
+app.use("/api/v1", postRoute);
+
+//routes
+app.use("/", (req, res) => {
+  try {
+    res.send("hlw from the other side....");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
 //listen port
-app.listen(port , ()=> {
-    console.log(`Server is running on PORT ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
